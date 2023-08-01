569ab80bc5381b9466a764c5f9ab722ecd3d09c4
jaivik chopda <jaivikchopda188@gmail.com>
2023-08-01 20:59:19 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "7162bd61e071c4c3b2429571a77f7dc6"
+    }
+}
+
+module.exports = constants;
